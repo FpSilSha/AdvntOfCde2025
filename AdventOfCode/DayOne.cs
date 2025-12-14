0cf82a07d553e8b62fe9ba0530fc8cb96ae3ba3e
Silv <faperez38thesildev@gmail.com>
2025-12-14 13:31:25 -0600

    Additions made to the code to count each 'click'. A catch also added for a 'click' and landing on 0, to prevent double increment with new logic

--- AdventOfCode/DayOne.cs
+++ AdventOfCode/DayOne.cs
@@ -8,17 +8,17 @@ namespace AdventOfCode
 {
     internal class DayOne
     {
-        
+
 
         public void Run()
         {
             try 
             {
-                // Pull in the txt of all the rotations and how far they will rotate
+                // Pull in the txt, line by line, of all the rotations and how far they will rotate
                 //StreamReader input = new StreamReader("..\\..\\..\\DayOnePuzzleInput.txt");
                 StreamReader input = new StreamReader("..\\..\\..\\DayOnePuzzleInput.txt");
                 string line;
-                int start = 50;
+                int start = 50; //22 for sample
                 int currentPlacement = start;
                 int countOfZero = 0;
                 //For logging purposes
@@ -32,10 +32,11 @@ namespace AdventOfCode
 
                     if ((line.Substring(0, 1)) == "R")
                     {
-                        // We know the constraints are X___ at max. There isn't a rotation greater than 999.
+                        // We know the constraints are X___ (3 digits) at max. There isn't a rotation greater than 999.
                         if (line.Length > 3)
                         {
                             currentPlacement += int.Parse(line.Substring(2));
+                            countOfZero += int.Parse(line.Substring(1, 1));
                         }
                         else
                         {
@@ -43,13 +44,19 @@ namespace AdventOfCode
                             currentPlacement += int.Parse(line.Substring(1));
                         }
 
-                        if (currentPlacement > 99) currentPlacement -= 100;
+                        if (currentPlacement >= 100)
+                        {
+                            currentPlacement -= 100;
+                            //This prevents double dipping with the final increment for case '0'
+                            if ((start != 100) && (currentPlacement != 0)) countOfZero++;
+                        }
 
                     } else
                     {
                         if (line.Length > 3)
                         {
                             currentPlacement -= int.Parse(line.Substring(2));
+                            countOfZero += int.Parse(line.Substring(1, 1));
                         }
                         else
                         {
@@ -58,14 +65,20 @@ namespace AdventOfCode
                         }
 
                         // currentPlacement could be negative. So we take 100 and 'add' the negative number it is at to get the abs val.
-                        if (currentPlacement < 0) currentPlacement = 100 + currentPlacement;
+                        if (currentPlacement < 0) 
+                        { 
+                            currentPlacement = 100 + currentPlacement;
+                            //
+                            if (start != 0) countOfZero++;
+
+                        }
                     }
                     if (currentPlacement == 0) countOfZero++;
+                    start = currentPlacement;
                     //For logging purposes
-                    //Console.WriteLine($"Now at {currentPlacement}. Zero counter is {countOfZero}");
+                    //Console.WriteLine($"Now at {currentPlacement}. Zero counter is {countOfZero}");//Zero counter is {countOfZero}
                     //Console.WriteLine($"Line used {lineNumber}");
-                    //Console.WriteLine("------------------------ \n\n\n\n");
-                    //Console.WriteLine(line);
+                    //Console.WriteLine("------------------------ \n\n");
                     //lineNumber++;
                 }
                 Console.WriteLine(countOfZero);
